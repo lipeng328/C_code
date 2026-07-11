f09c93df7bcdff75d9688536030977107e3161d3
lipeng328 <lipeng05120@outlook.com>
2026-07-11 09:27:55 +0800

    添加项目文件。

---
+++ class01/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define PI 3.1415926
+
+int main()
+{
+	printf("%zu\n", strlen(arr1));
+
+	return 0;
+}
